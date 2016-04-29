d4d59914dc86f7f78336ad238c40835ae66f82b6
ORIus <orius@mail.ua>
2016-04-29 15:27:13 +0300

    countBirs

--- src/codegym/CountBits.java
+++ src/codegym/CountBits.java
@@ -7,16 +7,14 @@ package codegym;
  */
 public class CountBits {
   public int count(int num) {
-
-      int bit = 1;
       
       int j = 0;
       for(int i=0; i<32; i++)
       {
-          if((num & bit) != 0)
+          if((num & 1) != 0)
             j++;
           
-          bit <<= 1;
+          num >>= 1;
       }
       
       return j;
