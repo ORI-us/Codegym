3f724be3bf1161813ff11fccddda046b786c14d3
ORIus <orius@mail.ua>
2016-05-22 18:14:51 +0300

    p 5_1

--- src/codegym/KmpSmallestPeriod.java
+++ src/codegym/KmpSmallestPeriod.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+A character string is said to have period k if it can be formed by concatenating one or more repetitions 
+of another string of length k. For example, the string "abcabcabcabc" has period 3, since 
+it is formed by 4 repetitions of the string "abc". It also has periods 6 (two repetitions of "abcabc")
+and 12 (one repetition of "abcabcabcabc"). 
+Write a program to read a character string and determine its smallest period Return (String) substring;
  */
 package codegym;
 
